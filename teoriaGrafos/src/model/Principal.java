a9b2bdf68a766d41848da179968a9757f2d18e7b
Victor <>
2016-10-04 09:46:04 -0300

    Faltando baixar a biblioteca

---
+++ teoriaGrafos/src/model/Principal.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Principal {
+
+	public static void main(String[] args) {
+             
+             Disciplina d1 = new Disciplina ("Matematica", "10/07/2016", "6periodo");
+             Disciplina d2 = new Disciplina ("Matematica", "10/07/2016", "8periodo");
+             Aluno a1 = new Aluno ("Jose", "10/07/2016", d1);
+             
+             XStream xstream = new XStream();
+             
+             xstream.alias("disciplina", Disciplina.class);
+             xstream.alias("aluno", Aluno.class);
+             
+             String xml = xstream.toXML(a1);
+             
+             System.out.println(xml);
+             a1 = null;
+             a1 = (Aluno) xstream.fromXML(xml);
+             System.out.println("==>" + a1.getNomeAluno());
+             
+        
+        }
+    
+}
+
